ac5c9c26cdf7ad4d58475370f903d577586b15df
Russell Sim <russell.sim@gmail.com>
2013-03-28 19:43:08 +1100

    updated setup for pypi

--- setup.py
+++ setup.py
@@ -1,20 +1,31 @@
 from setuptools import setup, find_packages
+from os import path
 
 version = '0.1'
+
+changelog_header = """
+Changelog
+=========
+
+"""
+
+desc_file = path.join(path.dirname(__file__), "README.rst")
+changelog_file = path.join(path.dirname(__file__), "CHANGELOG.rst")
+description = open(desc_file).read() + changelog_header + open(changelog_file).read()
+
 
 setup(name='bb-pytest',
       version=version,
       description="Buildbot step for py.test.",
-      long_description="""\
-""",
+      long_description=description,
       classifiers=[],  # Get strings from
                        #http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='',
       author='Russell Sim',
       author_email='russell.sim@gmail.com',
-      url='',
+      url='https://github.com/russell/bb-pytest',
       license='GPL',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      packages=find_packages(exclude=['tests']),
       include_package_data=True,
       zip_safe=False,
       install_requires=[
